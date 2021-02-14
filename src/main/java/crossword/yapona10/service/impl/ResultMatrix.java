a84677be4f864ff0da0729921b0b07b3c7675b54
LogosITAcademy <Tester99037@gmail.com>
2021-02-14 09:52:44 +0200

    indents work

--- src/main/java/crossword/yapona10/service/impl/ResultMatrix.java
+++ src/main/java/crossword/yapona10/service/impl/ResultMatrix.java
@@ -28,13 +28,15 @@ public class ResultMatrix {
 	public byte[][] displacementVert(List<List<Integer>> rows, int sizeColumn) {
 
 		byte[] handler = new byte[sizeColumn];
-		Boolean finish = new Boolean(false);
+		List<Integer> indents = new ArrayList<>();
 		
 		for (int row = 0; row < rows.size(); row++) {
-
-			handler = displacement(rows.get(row), sizeColumn, finish, 1);
 			
-			System.out.println(" finish   --------------- " + finish);
+			createIndents(rows.get(row), sizeColumn);
+			
+			indents.clear();
+			indents = createIndents(rows.get(row), sizeColumn);	
+			handler = displacement(rows.get(row), sizeColumn, indents);	
 			
 			for (int i = 0; i < handler.length; i++) {
 				result[row][i] = handler[i];
@@ -45,15 +47,11 @@ public class ResultMatrix {
 
 	public byte[][] displacementHorz(List<List<Integer>> columns, int sizeRow) {
 
-		byte[] handler = new byte[sizeRow];
-		
+		byte[] handler = new byte[sizeRow];		
 		
 		for (int column = 0; column < columns.size(); column++) {
-			
-			
 
-			//handler = displacement(columns.get(column), sizeRow, finish, 1);
-			
+			//handler = displacement(columns.get(column), sizeRow, finish, 1);			
 
 			for (int i = 0; i < handler.length; i++) {
 				result[i][column] = handler[i];
@@ -61,39 +59,66 @@ public class ResultMatrix {
 		}
 		return result;
 	}
-
-	public byte[] displacement(List<Integer> rowOrColumn, int sizeColumnOrRow, 
-				Boolean finish, int numElement) {
+	
+	
+	public List<Integer> createIndents(List<Integer> rowOrColumn, int sizeColumnOrRow) {
 		
-		numElement = rowOrColumn.size() - 1;
+		List<Integer> indents = new ArrayList<>();
+		int summLengthElement = 0;
+		int summIndents = 0;
+		int indent = 0;
+		
+		for (int indInnerList = 0; indInnerList < rowOrColumn.size(); indInnerList++) {
+			summLengthElement += rowOrColumn.get(indInnerList);
+			if(summLengthElement + summIndents <= sizeColumnOrRow) {
+				indent = 1;
+				
+				if(indInnerList == rowOrColumn.size() - 1) {
+					indent = sizeColumnOrRow - summLengthElement - summIndents;
+				}
+				
+				summIndents += indent;
+				indents.add(indent);
+			}			
+		}
+		
+//		System.out.println(rowOrColumn);
+//		System.out.println(indents);
+		
+		return indents;		
+	}
+	
 
+	public byte[] displacement(List<Integer> rowOrColumn, int sizeColumnOrRow, List<Integer> indents) {
+		
 		byte[] handler = new byte[sizeColumnOrRow];
 
 		// pass along the line vertical or horizontal push into the matrix from the
 		// beginning with a single gap
 
 		int lengthElement = 0;
+		int lengthIndent = 0;
 		int indMatrix = 0;
 		int indexExit = 0;
 		for (int indInnerList = 0; indInnerList < rowOrColumn.size(); indInnerList++) {
 
 			lengthElement = rowOrColumn.get(indInnerList);
+			lengthIndent = indents.get(indInnerList);
+			
 			indexExit = indMatrix + lengthElement;
 
 			while (indMatrix < indexExit) {
 				handler[indMatrix++] = 1;
 			}
-
-			if (indMatrix < sizeColumnOrRow) {
-				 do {
+			
+			indexExit += lengthIndent;
+			
+			if (indMatrix < sizeColumnOrRow || indexExit < sizeColumnOrRow) {
+				while (indMatrix < indexExit) {
 					handler[indMatrix++] = 2;
-				 }while(indMatrix < sizeColumnOrRow && indInnerList == numElement);
+				}
 			}
-		}
-		finish = true;
-		
-		System.out.println(" rrr vfvf finish   --------------- " + finish);
-		
+		}		
 		return handler;
 	}
 
